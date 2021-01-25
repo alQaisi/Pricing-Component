913ceef01d8f1b19143064bd4f9c818f36f95eab
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2021-01-25 13:57:19 +0200

    fixing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const dollars=document.getElementById("dollars");
 const yearlyBilling=document.getElementById("yearlyBilling");
 const pageviews=document.getElementById("pageviews");
 const pageviews2=document.getElementById("pageviews2");
+const billingType=document.getElementById("billingType");
 const prices=[["10K PAGEVIES",8],
               ["50K PAGEVIES",12],
               ["100K PAGEVIES",16],
@@ -12,11 +13,12 @@ let applyDiscount=false;
 let value=40;
 yearlyBilling.addEventListener("change",(evt)=>{
     applyDiscount=evt.srcElement.checked;
+    applyDiscount?billingType.innerText=`/ year`:billingType.innerText=`/ month`;
     updateCost(value);
 })
 const updateCost=(value)=>{
     if(applyDiscount==true){
-        price=prices[value/20][1]-prices[value/20][1]*0.25;
+        price=(prices[value/20][1]-prices[value/20][1]*0.25)*12;
         dollars.innerText=`$${price}.00`;
     }
     else
